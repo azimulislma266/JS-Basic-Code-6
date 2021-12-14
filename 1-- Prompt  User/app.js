54725279f7f037f1006637029d0930f90ac15573
Azimul Islam <86977364+azimulislma266@users.noreply.github.com>
2021-12-14 09:09:39 +0600

    Add files via upload

---
+++ 1-- Prompt  User/app.js
@@ -0,0 +1,21 @@
+// let age = prompt("What is your age","Tomar boyso 18 + Hote hobe");
+
+// document.write(`" Your age ${age} year old"`);
+
+
+
+
+// User ar kaj thke ki babe data ane variabel ar moddo joma rakha dispaly korano
+ // alhamdullha ayta bujlam nejar theke kori 
+
+
+
+//  const Tf = confirm("Are You Sure");
+
+//   const Rejult = (Tf==true)? window.location = "https://www.google.com": alert("Thanks For Wathing");
+
+
+
+// alhamdulla bujta parlam
+
+
---
+++ 13-- Arry valou chinge/app.js
@@ -0,0 +1,8 @@
+
+
+let arryValou = [20,"Aynal Hok", 10, "42", "OguTomar", 10,];
+
+
+arryValou[1] = "azimulislam";
+
+document.write(arryValou[5]); // alhamdulla bujta parlam
